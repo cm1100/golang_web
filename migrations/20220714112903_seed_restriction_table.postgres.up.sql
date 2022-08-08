2154df7ea90beef0d3e67873be09c2ecc5617e9d
Chaitanya Malik <timeisgod12@gmail.com>
2022-08-08 14:03:44 +0530

    new added

--- migrations/20220714112903_seed_restriction_table.postgres.up.sql
+++ migrations/20220714112903_seed_restriction_table.postgres.up.sql
@@ -1,4 +1,3 @@
-/*INSERT INTO "restrictions" ("id", "restriction_name", "created_at", "updated_at") VALUES
+INSERT INTO "restrictions" ("id", "restriction_name", "created_at", "updated_at") VALUES
 (1,	'Reservation',	'2022-07-10 10:58:32.091542',	'2022-07-10 10:58:32.091542'),
 (2,	'Owner Block',	'2022-07-10 13:38:42.000264',	'2022-07-10 13:38:42.000264');
-*/
